08a5e955678486936e16ef6a90f34ccabfa0e462
makmm <makmmelmejorenlacompu@gmail.com>
2017-12-18 14:43:10 -0300

    organize code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const LOG_PREFIX = '[main] '
+const mongo = require('./config/mongo.js')
+const express = require('./config/express.js')
 
 const log = require('./utils/logger.js').log
 
-require('./config/mongo.js')()
-  .then(require('./config/express.js'))
-  .then(() => {
-    log(LOG_PREFIX + 'Started.')
-  })
-  .catch(err => {
-    log(LOG_PREFIX + 'Something errored...terminating')
-  })
+const LOG_PREFIX = '[main] '
+
+Promise.all([
+	mongo(),
+	express()
+])
+	.then(() =>
+		log(LOG_PREFIX + 'Started.'))
+	.catch(err =>
+		log(LOG_PREFIX + 'Something errored...terminating'))
