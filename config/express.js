16002c42b6b6dd0f70795b522becadb5f2aad059
makmm <makmmelmejorenlacompu@gmail.com>
2018-01-01 14:48:57 -0300

    cross origin localhost:3000 > 8080 (should I set it to *?)

--- config/express.js
+++ config/express.js
@@ -15,7 +15,7 @@ module.exports = () => new Promise(async (resolve, reject) => {
 
   // Fix for cross-origin
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     res.header("Access-Control-Allow-Credentials", "true")
     next()
