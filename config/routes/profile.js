75849b4f65d4627a19b5f95bda09838aa85916a2
makmm <makmmelmejorenlacompu@gmail.com>
2018-03-01 14:41:12 -0300

    give _id with profile

--- config/routes/profile.js
+++ config/routes/profile.js
@@ -5,11 +5,12 @@ module.exports = () => {
       res.status(200).json({
         success: true,
         profile: {
+          _id: req.user._id,
           username: req.user.username,
           email: req.user.email
         }
       })
     }
-    
+
   }
 }
